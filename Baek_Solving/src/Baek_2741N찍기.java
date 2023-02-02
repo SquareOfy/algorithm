56e54100a61413de8761ac28021ff3deb3258e52
Your Name <you@example.com>
2023-02-03 01:10:46 +0900

    2023-02-02

---
+++ Baek_Solving/src/Baek_2741N찍기.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Baek_2741N찍기 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		for(int i=1; i<=N; i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
