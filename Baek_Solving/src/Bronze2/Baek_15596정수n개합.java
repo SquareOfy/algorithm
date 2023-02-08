dbbfeee7be2a1a31ec6f45b5faf3c24055e9ebb4
황윤영 <y6357@naver.com>
2023-02-08 17:52:51 +0900

    2023-02-08

---
+++ Baek_Solving/src/Bronze2/Baek_15596정수n개합.java
@@ -0,0 +1,15 @@
+package Bronze2;
+
+public class Baek_15596정수n개합 {
+
+	long sum(int[] a) {
+		long ans =0;
+		for (int i =0; i< a.length; i++) {
+			ans += (long) a[i];
+		}
+		return ans =0;
+		
+	
+	}
+
+}
