be17ef3ab14974e910c583cf70ac420df6e47467
황윤영 <y6357@naver.com>
2023-02-13 23:14:00 +0900

    2023-02-13 집

---
+++ Baek_Solving/src/Silver4/Baek_1244스위치켜고끄기.java
@@ -0,0 +1,10 @@
+package Silver4;
+
+public class Baek_1244스위치켜고끄기 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ SwExpert_Solving/src/D3/SE_10505소득불균형.java
@@ -0,0 +1,19 @@
+package D3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SE_10505소득불균형 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for(int t=1; t<=T; t++) {
+			int N = Integer.parseInt(br.readLine());
+			String[] numbers = br.readLine().split(" ") 
+		}
+
+	}
+
+}
