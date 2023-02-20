ccfea4197203af84305edc8577118e120b4bb403
황윤영 <y6357@naver.com>
2023-02-20 22:53:04 +0900

    체스판 채우기 더럽게 안풀린다

---
+++ Baek_Solving/src/Silver4/Baek_10158개미시초를피하자.java
@@ -0,0 +1,31 @@
+package Silver4;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Baek_10158개미시초를피하자 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		int w = Integer.parseInt(st.nextToken()); int h = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		int p = Integer.parseInt(st.nextToken()); int q = Integer.parseInt(st.nextToken());
+	
+		long t = Integer.parseInt(br.readLine());
+		
+		
+		
+
+	}
+
+}
